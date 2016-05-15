bf125637238f5583c42cbccb6e5fb7aae02411f4
Chase Miller <chmille4@gmail.com>
2016-05-14 20:32:56 -0400

    add mutations for large deletion and frameshift deletion

--- evolutionaryart.js
+++ evolutionaryart.js
@@ -8,7 +8,11 @@ fs.createReadStream('images/input/monkey.png')
     }))
     .on('parsed', function() {
 
-        this.data = evoart.mutate(this, 'uv', '8bit');
+        // large in frame deletion
+        // this.data = evoart.mutate(this, 'deletion', '24bit', {deletionPercent: 0.01});
+
+        // small potentially frameshift deletion
+        this.data = evoart.mutate(this, 'frameshift', '24bit', {deletionBases: 4});
 
         // convert to dna and back again, should get original image
         // this.data = evoart.dna2Img( evoart.img2Dna(this, '8bit'), '8bit' );
